b15ec80bad2d7ddc589067196becae89363457a1
Felix-Li-0306 <229174898+Felix-Li-0306@users.noreply.github.com>
2026-03-09 13:09:02 +0800

    setup: add initial Xcode iOS app project

---
+++ iOSMarkdownEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSMarkdownEditor
+//
+//  Created by 李卓非 on 9/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSMarkdownEditor/iOSMarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMarkdownEditorApp.swift
+//  iOSMarkdownEditor
+//
+//  Created by 李卓非 on 9/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOSMarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
